5d5cbf019ba847c959ef46882be5a10f860095c5
Evert Lagerberg <evert.lagerberg@schibsted.se>
2016-05-27 17:29:33 +0200

    changed name to bar

--- source/javascripts/preview_chart.js
+++ source/javascripts/preview_chart.js
@@ -90,7 +90,7 @@ $(document).ready(function() {
 
   pymChild.onMessage('updateData', function(data) {
     console.log('----------- received message', data);
-    data = JSON.parse(data.data);
+    data = JSON.parse(data);
     create_Chart();
   })
 
